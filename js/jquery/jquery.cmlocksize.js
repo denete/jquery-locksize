4bfb6dc92c6e45e97a160be61ac36ad6e763c01d
denete <david.enete@cmgdigital.com>
2011-02-12 09:39:22 -0500

    initial commit

---
+++ js/jquery/jquery.cmlocksize.js
@@ -0,0 +1,30 @@
+(function($) {
+
+    $.fn.cmlocksize = function(options) {
+        var opts = $.extend({},
+        $.fn.cmlocksize.defaults, options);
+        initLocksize(opts.targetID);
+
+        return this.each(function() {
+        });
+    };
+
+    // private function for initializing
+    function initLocksize(id) {
+        //nothing
+        }
+    // private function for debugging
+    function debug(data) {
+        if (window.console && window.console.log) {
+            window.console.log(data.replace(/<br>/g, "\n"));
+        } else {
+            alert(data.replace(/\n/g, "<br>"));
+        }
+    };
+
+    // set defaults for the plugin
+    $.fn.cmlocksize.defaults = {
+        targetID: ""
+    };
+
+})(cmg.query);

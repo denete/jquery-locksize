ddeadcfdb79417bba7728b6393cdc940b7de1f60
denete <david.enete@cmgdigital.com>
2011-02-14 12:31:27 -0500

    further progress on the plugin, lots of changes made

--- js/jquery/jquery.cmlocksize.js
+++ js/jquery/jquery.cmlocksize.js
@@ -2,27 +2,52 @@
     
     var methods = {
         init: function(options) {
-            var opts = $.extend({},
-            $.fn.cmlocksize.defaults, options);
-            initLocksize(opts.targetID);
+            //opts = $.extend({}, $.fn.cmlocksize.defaults, options);
+            return this.each(function() {
+                var $this = $(this);
+                data = $this.data('cmlocksize');
+                if(!data){
+                    console.log('init, data: ' + data);
+                    $this.wrap('<div id="cmLockContainer"><div id="cmLockSubContainer" /></div>');
+                    $('#cmLockContainer').css({width: $(this).width(),height: $(this).height()});
+                    opts = $.extend({}, $.fn.cmlocksize.defaults, options);
+                    $this.data('cmlocksize', opts);
+                }
+            });
+        },
+        destroy: function(){
+            return this.each(function(){
+                var $this = $(this);
+                data = $this.data('cmlocksize');
+                //$(window).unbind('.???');
+                $this.unwrap().unwrap();
+                data.cmlocksize.remove();
+                $this.removeData('cmlocksize');
+            })
         },
         lockOn: function(){
             //not sure if this next block goes in init or here
-            return this.each(function() {
-                var $this = $(this);
-                $this.wrap('<div id="cmLockContainer"><div id="cmLockSubContainer" /></div>');
-            });
 
             prevHeight = cmg.query('#cmLockSubContainer').height();
             prevWidth = cmg.query('#cmLockSubContainer').width();
-            console.log('prevHeight: ' + prevHeight + ', prevWidth: ' + prevWidth);
+            //prevHeight = $(this).height();
+            //prevWidth = $(this).width();
+            //console.log('prevHeight: ' + prevHeight + ', prevWidth: ' + prevWidth);
             //console.log("subcontainer prevHeight: " + prevHeight);
 
-            cmg.query('#cmLockContainer').css({height: prevHeight, width: prevWidth});
-            pointer++;
-            pointer = pointer == images.length ? 0 : pointer;
-
-
+            $(this).fadeTo(opts.animSpeed, 0, function(){
+                $('#cmLockContainer').css({height: prevHeight, width: prevWidth});
+                opts.onLock.call(this);
+                $(this).fadeTo(opts.animSpeed, 1, function(){
+                    $('#cmLockContainer').animate({
+                        width: $(this).width(),
+                        height: $(this).height()
+                    }, 500, function() {
+                        //cmg.query('#cmImageContainer').css('height','auto');
+                    });
+                });
+            });
+            /*
             cmg.query('#cmImage').fadeTo(500, 0, function () {
                 cmg.query(this).attr('src','img/' + images[pointer]);
                 cmg.query(this).fadeTo(500, 1);
@@ -33,26 +58,35 @@
                     //cmg.query('#cmImageContainer').css('height','auto');
                 });
             });
+            */
         },
-        lockOff: function( ) {}
+        lockOff: function(){
+            //console.log('new width:' + $(this).width());
+            /*$('#cmLockContainer').animate({
+                width: $(this).width(),
+                height: $(this).height()
+            }, 500, function() {
+                $(this).fadeTo(opts.animSpeed, 1);
+            });*/
+            $(this).fadeTo(opts.animSpeed, 1, function(){
+                $('#cmLockContainer').animate({
+                    width: $(this).width(),
+                    height: $(this).height()
+                }, 500, function() {
+                    //cmg.query('#cmImageContainer').css('height','auto');
+                });
+            });
+        }
       };
 
     $.fn.cmlocksize = function(method) {
         if ( methods[method] ) {
+            //console.log('method: ' + method);
             return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
         } else if ( typeof method === 'object' || ! method ) {
             return methods.init.apply( this, arguments );
         } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.cmlocksize' );
-        }
-    };
-
-    // private function for debugging
-    function debug(data) {
-        if (window.console && window.console.log) {
-            window.console.log(data.replace(/<br>/g, "\n"));
-        } else {
-            alert(data.replace(/\n/g, "<br>"));
+            console.log( 'Method ' +  method + ' does not exist on jQuery.cmlocksize' );
         }
     };
 
@@ -60,7 +94,8 @@
     $.fn.cmlocksize.defaults = {
         animSpeed: 500,
         delay: 250,
-        targetID: ""
+        targetID: "",
+        onLock: function(){}
     };
 
 })(cmg.query);

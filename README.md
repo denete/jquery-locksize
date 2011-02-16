51374851589c7a7f44fb27c3b06048dbda0fffbb
David Enete <david@enete.org>
2011-02-16 14:41:13 -0500

    First pages commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Locksize plugin for jQuery
+===========================
+
+A jQuery plugin to allow elements on a page to be switched out in an AJAXy manner without having screen jump due to elements being removed and restored, etc. It will work with any page element, but has not been tested on mass instances. 
+
+Locksize 1.0.0 requires jQuery 1.4 or higher.
+
+***
+
+## Usage:
+
+>        $('#cmImage').cmlocksize({
+>                animSpeed: 250, // speed of fade and resize animations in milliseconds
+>                delay: 100, // delay before resizing in milliseconds
+>                onLock: function(){ // a function to be run once the element size is locked
+>                        $(this).attr('src','img/' + images[pointer]);
+>                    },
+>                onUnlock: function(){ // a function to be run once the resizing has finished
+>                    //nothing
+>                    },
+>                fade: true, // fade the element out / in and animate the resize * true is recommended for current code *
+>                autoUnlock: true // if set to false, you can call the unlock method when you want to
+>                });
+
+## Methods:
+
+>        $('#cmImage').cmlocksize('lockOn'); // instantiate the lock
+>        $('#cmImage').cmlocksize('lockOff'); // programmatically release the lock
+>        $('#cmImage').cmlocksize('destroy'); // destroy the instance of cmlocksize
---
+++ js/jquery/jquery.cmlocksize.js
@@ -0,0 +1,81 @@
+/**
+ * Plugin: jQuery Locksize, jquery.cmlocksize.js
+ * Copyright: Copyright (c) 2011 CMGdigital
+ * Version: 1.0.0
+ * Author: David A. Enete
+ * Date: 2011-02-14
+ * Description: jQuery Locksize plugin - holds the size of an element on a page while the element is modified then the lock is released
+ */
+
+(function($) {
+    
+    var methods = {
+        init: function(options) {
+            return this.each(function() {
+                $this = $(this);
+                data = $this.data('cmlocksize');
+                if(!data){
+                    $this.wrap('<div class="cmLockContainer" style="overflow:hidden;"><div class="cmLockSubContainer" style="overflow:hidden;" /></div>');
+                    $this.parents('.cmLockContainer').css({width: $this.width(),height: $this.height()});
+                    opts = $.extend({}, $.fn.cmlocksize.defaults, options);
+                    $this.data('cmlocksize', opts);
+                }
+            });
+        },
+        destroy: function(){
+            return this.each(function(){
+                $this = $(this);
+                data = $this.data('cmlocksize');
+                if(data){
+                    $this.unwrap().unwrap();
+                    $this.removeData('cmlocksize');
+                }
+            })
+        },
+        lockOn: function(){
+            prevHeight = $this.parent().height();
+            prevWidth = $this.parent().width();
+            //if(!opts.fade) opts.animSpeed = 100;
+
+            $this.fadeTo(opts.animSpeed, 0, function(){
+                $this.parents('.cmLockContainer').css({height: prevHeight, width: prevWidth});
+                opts.onLock.call(this);
+                
+                if(opts.autoUnlock){
+                    $this.cmlocksize('lockOff');
+                }
+            });
+        },
+        lockOff: function(){
+            $this.delay(opts.delay).fadeTo(opts.animSpeed, 1, function(){
+                $this.parents('.cmLockContainer').animate({
+                    width: $this.width(),
+                    height: $this.height()
+                }, opts.animSpeed, function() {
+                    opts.onUnlock.call(this);
+                });
+            });
+        }
+    };
+
+    $.fn.cmlocksize = function(method) {
+        if ( methods[method] ) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            console.log( 'Method ' +  method + ' does not exist on jQuery.cmlocksize' );
+        }
+    };
+
+    // set defaults for the plugin
+    $.fn.cmlocksize.defaults = {
+        animSpeed: 500,
+        delay: 250,
+        onLock: function(){},
+        onUnlock: function(){},
+        fade: true,
+        autoUnlock: true
+    };
+
+})(cmg.query);
